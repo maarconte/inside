3fd89ea322cbd8afda4ab516b9d57d271ef78a7d
Mathilde Arconte <signup@marconte.fr>
2021-09-14 17:00:51 +0200

    responsive header

--- header.php
+++ header.php
@@ -57,11 +57,22 @@
                 <h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
             </header>
 <?php endif; ?>
-<?php if ( is_front_page()): ?>
+<?php if ( is_front_page()) : ?>
             <header class="page-header page-header_front-page">
               <img class="page-header_logo" src="<?php if($image[0]){ echo $image[0];}?>" alt="<?php echo get_bloginfo('name')?>">
 
                 <!-- <img class="clip-svg" src="//localhost:3000/wp-content/uploads/2021/08/MicrosoftTeams-image.jpg" alt="<?php // echo get_bloginfo('name')?>"> -->
+<div class="site-header clip-svg"></div>
+                <svg width="0" height="0">
+                  <clipPath id="svgClip" clipPathUnits="objectBoundingBox" class="svg-clip">
+                    <path d="M16.91,0,.23,313c-4.36,82,53.12,152.94,130,160.5l713,70.12c84.11,8.27,156.7-62.1,156.7-151.88V0Z"></path>
+                  </clipPath>
+                </svg>
+            </header>
+
+<?php elseif ( is_single() || is_singular()) : ?>
+            <header class="page-header page-header_front-page">
+            <h1 class="page-title"><?php the_title(); ?></h1>
 <div class="site-header clip-svg"></div>
                 <svg width="0" height="0">
                   <clipPath id="svgClip" clipPathUnits="objectBoundingBox" class="svg-clip">
